b8c39df7aa4e3f24b559b56aefd19c230e3803b9
brect <b.limas93@gmail.com>
2020-07-01 20:24:56 -0300

    Service layer, component registration

--- src/main/java/com/blimas/course/resources/UserResource.java
+++ src/main/java/com/blimas/course/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.blimas.course.resources;
 
 import com.blimas.course.entities.User;
+import com.blimas.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User();
+    public ResponseEntity<List<User> > findAll(){
+        List<User> listUsers = userService.findAll();
+        return ResponseEntity.ok().body(listUsers);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }

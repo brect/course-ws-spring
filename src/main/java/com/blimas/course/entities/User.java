8eda5f40971cb25d014b65116b8363a02792743c
brect <b.limas93@gmail.com>
2020-06-30 23:54:48 -0300

    adicionado h2 e repository

--- src/main/java/com/blimas/course/entities/User.java
+++ src/main/java/com/blimas/course/entities/User.java
@@ -1,10 +1,17 @@
 package com.blimas.course.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;

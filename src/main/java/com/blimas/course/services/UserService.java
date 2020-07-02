9626883ca4244a11862762eafbb575a627135682
brect <b.limas93@gmail.com>
2020-07-01 23:33:21 -0300

    user create and delete

--- src/main/java/com/blimas/course/services/UserService.java
+++ src/main/java/com/blimas/course/services/UserService.java
@@ -23,4 +23,12 @@ public class UserService {
         return user.get();
     }
 
+    public User insert(User user){
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+
 }
